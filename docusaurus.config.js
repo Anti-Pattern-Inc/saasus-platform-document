4e3f93ed2d1655fbe1d559959c002e997c281b81
takashi-uchida <takashi.uchida.office@gmail.com>
2024-02-26 14:44:01 +0900

    fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   url: 'https://docs.saasus.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/saasus-platform-document/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
