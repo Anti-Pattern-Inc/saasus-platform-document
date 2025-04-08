3314e63fb352e6cbfb6e89cbcdf5a7c43ddd9875
jSasaki <takatsugu.sasaki@anti-pattern.co.jp>
2025-04-08 19:21:24 +0900

    remove java from Prism additionalLanguages due to build error with triple-quoted-string

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -190,7 +190,7 @@ const config = {
       prism: {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
-        additionalLanguages: ["php", "csharp", "java"],
+        additionalLanguages: ["php", "csharp"],
       },
     }),
 
