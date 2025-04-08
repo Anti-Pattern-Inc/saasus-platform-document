989e0e4ec126d71d95a340c42997819127a4262c
jSasaki <takatsugu.sasaki@anti-pattern.co.jp>
2025-04-08 17:01:05 +0900

    add csharp and java to Prism additionalLanguages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -190,7 +190,7 @@ const config = {
       prism: {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
-        additionalLanguages: ["php"],
+        additionalLanguages: ["php", "csharp", "java"],
       },
     }),
 
